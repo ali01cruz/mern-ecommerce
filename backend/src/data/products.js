74c6c8868543eb8c26c30e902985a7e918048eb1
abel bustamante <abeldevelop123@gmail.com>
2022-01-27 11:12:47 -0300

    agregado de seeder , hassPassword, products., users

---
+++ backend/src/data/products.js
@@ -0,0 +1,36 @@
+
+const products = [
+    {
+        name:'Sample name',
+        price:0,
+        image:'/image/',
+        brand:'Sample brand',
+        category:'Sample category',
+        contInStock:0,
+        numReviews:0,
+        description:'Sample description',
+    },
+    {
+        name:'Sample name2',
+        price:0,
+        image:'/image/',
+        brand:'Sample brand2',
+        category:'Sample category2',
+        contInStock:0,
+        numReviews:0,
+        description:'Sample description2',
+    },
+    {
+        name:'Sample name3',
+        price:0,
+        image:'/image/',
+        brand:'Sample brand3',
+        category:'Sample category3',
+        contInStock:0,
+        numReviews:0,
+        description:'Sample description3',
+    },
+    
+]
+
+export default products;
